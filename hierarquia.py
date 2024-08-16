42035d6147c49b789e2a8d260231feed9a768b5e
DavidLLucas <vidal.david@escola.pr.gov.br>
2024-08-16 12:13:07 -0300

    first commit

---
+++ hierarquia.py
@@ -0,0 +1,28 @@
+import time
+
+print("Seja Bem vindo")
+name = input("Informe seu nome")
+print("Bem vindo,", name + "!")
+
+print("Como posso te ajudar ?")
+
+print("1-Guns N'Roses")
+print("2-Metallica")
+print("3-Rammstein")
+
+opcao = int(input("Digite o número da opção desejada:"))
+
+if opcao == 1: 
+    print("Guns N'Roses")
+
+    elif opcao == 2:
+    print("Metallica")
+
+    elif opcao == 3:
+    print("Rammstein")
+
+    else:
+    print("Opção incorreta!")
+
+
+        
