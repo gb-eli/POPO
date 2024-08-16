4fb1473b149f3f88ceae094995daf53c3d1d4374
Gabriel Elienai <162762339+gb-eli@users.noreply.github.com>
2024-08-16 20:18:07 -0300

    Update and rename hierarquia.py to Fiz hierarquia.py
    tive que ajustar a indentação no bloco de condição 
    Outro ponto é que faltou criar a função menu e depois chamá-la sempre que necessario.
    Tents realizar os ajustes ou vemos na próxima aula

--- Fiz hierarquia.py
+++ Fiz hierarquia.py
@@ -15,14 +15,20 @@ opcao = int(input("Digite o número da opção desejada:"))
 if opcao == 1: 
     print("Guns N'Roses")
 
-    elif opcao == 2:
+elif opcao == 2:
     print("Metallica")
 
-    elif opcao == 3:
+elif opcao == 3:
     print("Rammstein")
 
-    else:
+else:
     print("Opção incorreta!")
+
+""" tive que ajustar a indentação no bloco de condição 
+Outro ponto é que faltou criar a função menu e depois chamá-la sempre que necessario.
+Tents realizar os ajustes ou vemos na próxima aula
+
+"""
 
 
         
